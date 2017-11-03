9914ba7e4f6a235f7083ac6694df4525138393f7
Louis-Axel Ambroise <ambroise.la@gmail.com>
2017-11-03 11:47:55 +0100

    Add confirmation in DownAll command

--- tools/DownAll.cmd
+++ tools/DownAll.cmd
@@ -2,6 +2,12 @@
 
 SET CONNECTIONNAME="FasTnT.Database"
 
+
+:PROMPT
+SET /P AREYOUSURE=All your tables and data will be deleted. Are you sure (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
 ..\packages\FluentMigrator.1.6.2\tools\Migrate.exe -a ../FasTnT.Data/bin/Debug/FasTnT.Data.dll -db Postgres -conn "%CONNECTIONNAME%" -t rollback:all
 
+:END
 pause
